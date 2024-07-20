33fea8d07324bc0a6b0cdd25a3525ce916ed00f1
tansophat <tansophat521@gmail.com>
2024-07-20 10:36:50 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+print("hello bbu112")
+
+print("I love 'Soy' in Pux ")
+print('I love "Pux" in Soy')
+print("I love \'Soy\' and \"Pux\" inside Pux")
+
+name=input("What's your name?")
+print("Hello welcome " +name)
+
+qty1=int(input("Input Qty1 : "))
+qty2=int(input("Input Qty2 : "))
+price=float(input("Input price : "))
+total=qty1+qty2
+amount=total*price
+print("Total Qty = " +str(total))
+print("Amount = " +str(amount))
+
+print("Welcome to the band name generator")
+city=input("Which city did you grow up in? ")
+pet=input("What is the name of a pet? ")
+print("YOur band name name could be " +city+" " +pet)
+
